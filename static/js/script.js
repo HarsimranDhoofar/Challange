645f5eaa9b7b770dc0061974a7db0f7d7b4c8c31
Harsimran Dhoofar <sam.dhoofar@gmail.com>
2020-03-27 16:05:56 -0400

    Part 1 Working

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,11 @@
-console.log("hello");
+//Challange 1: Your Age in Days
+
+function ageToDays() {
+    var yearBorn = prompt('Whay year you were born?');
+    var yearOldInDays = (2020 - yearBorn) * 365;
+    document.getElementById('displayAgeInDays').innerHTML = ' You are ' + yearOldInDays + ' old!'
+}
+
+function reset() {
+    document.getElementById('displayAgeInDays').remove();
+}
