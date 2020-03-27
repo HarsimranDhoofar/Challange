fe28cd2f63602de638ba891751160a697e053102
Harsimran Dhoofar <sam.dhoofar@gmail.com>
2020-03-27 18:48:32 -0400

    Part 2 Working

--- static/js/script.js
+++ static/js/script.js
@@ -9,3 +9,12 @@ function ageToDays() {
 function reset() {
     document.getElementById('displayAgeInDays').remove();
 }
+
+//Challange 2: Generate Cat
+function generateCat() {
+
+    var img = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    img.src = 'https://www.letstalkabout.co.uk/wp-content/uploads/2014/01/reaction-gif-sleepy-cute-cat.gif';
+    div.appendChild(img);
+}
